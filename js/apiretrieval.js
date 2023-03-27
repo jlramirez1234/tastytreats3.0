87a45e2d7132a98f1f57d89bced68edfd1a99705
Jonathan Ramirez <77942993+jlramirez1234@users.noreply.github.com>
2023-03-27 14:15:26 -0500

    refactor js

--- js/apiretrieval.js
+++ js/apiretrieval.js
@@ -1,4 +1,4 @@
-async function callingDesserts() {
+const callingDesserts = async () => {
   const routes = ["?category=Cookie", "?category=Donut", "?category=Ice_Cream"];
   const baseURL = "https://freerandomapi.cyclic.app/api/v1/desserts";
   const res = await Promise.all(
@@ -8,20 +8,26 @@ async function callingDesserts() {
         .then((response) => response.data)
         .then((data) =>
           data.map((info) => {
+            info.isHearted = false;
+            // console.log(info)
             return info;
           })
         )
     )
   );
+  return res;
+};
+
+async function dessertsToDisplay() {
+  let res = await callingDesserts();
   const goodies = document.querySelector(".goodies-option");
-  let card;
-  
+
   for (const item of res.flat()) {
-    card = document.createElement("div");
+    let card = document.createElement("div");
     card.classList.add("card-container");
     card.innerHTML = `
       <div class="icon-container">
-        <i id="myHeart" class="fa-regular fa-heart blue"></i>
+        <i id="myHeart" class="fa-regular fa-heart"></i>
       </div>
       <div class="category-text">
         <div class="image-container"><img src="${item.photoUrl}" alt="${item.name}"></div>
@@ -29,60 +35,70 @@ async function callingDesserts() {
         <h3 id="sweetsName">${item.description}</h3>
       </div>
     `;
-    const heart = card.querySelector(".fa-heart")
-    heart.addEventListener("click", function () {
-      if (heart.classList.contains("red")) {
-        heart.classList.remove("red");
-        heart.classList.add("blue");
-        // alert(`You have taken off ${item.name} from your favorites!`);
-        heartTurnsBlue();
+
+    const heart = card.querySelector(".fa-regular");
+
+    heart.addEventListener("click", () => {
+      if (!item.isHearted) {
+        item.isHearted = true;
+        heart.classList.add("is-heart");
+        card.remove();
+        favoritesPage(item, goodies);
       } else {
-        heart.classList.remove("blue");
-        heart.classList.add("red");
-        // alert(`You have added ${item.name} to your favorites!`);
-        heartTurnsRed(`${item.photoUrl}`, `${item.name}`, `${item.description}`);
+        item.isHearted = false;
+        heart.classList.remove("is-heart");
+        card.remove();
       }
     });
+
     goodies.appendChild(card);
   }
 }
 
-callingDesserts();
+dessertsToDisplay();
 
-function heartTurnsRed(photoUrl, name, description) {
-  const container = document.querySelector('.favorites-section');
-  const cardContent = document.querySelector('.content-area');
-
-  card = document.createElement('div');
-  card.classList.add('card-container');
+function favoritesPage(item, goodies) {
+  const cardContent = document.querySelector(".content-area");
+  let card = document.createElement("div");
+  card.classList.add("card-container");
   card.innerHTML = `
-    <div class="icon-container"><i id="myHeart" class="fa-regular fa-heart blue"></i></div>
+    <div class="icon-container"><i id="myHeart" class="fa-regular fa-heart is-heart"></i></div>
     <div class="category-text">
-    <div class="image-container"><img src="${photoUrl}" alt="${name}"></div>
-    <h1 id="category">${name}</h1>
-    <h3 id="sweetsName">${description}</h3>
+    <div class="image-container"><img src="${item.photoUrl}" alt="${item.name}"></div>
+    <h1 id="category">${item.name}</h1>
+    <h3 id="sweetsName">${item.description}</h3>
     </div>
-  `
+  `;
 
   cardContent.appendChild(card);
-  
-  // Add an event listener to the heart icon
-  const heart = card.querySelector('.fa-heart');
-  heart.addEventListener('click', heartTurnsBlue );
+
+  const heart = card.querySelector(".fa-regular");
+  heart.addEventListener("click", () => {
+    if (item.isHearted) {
+      item.isHearted = false;
+      card.remove();
+      heart.classList.remove("is-heart");
+      goodies.appendChild(card);
+    }
+  });
 }
 
-function heartTurnsBlue() {
-  // Remove the selected card
-  const cardContent = document.querySelector('.content-area');
-  const selectedCard = document.querySelector('.card-container');
-  cardContent.removeChild(selectedCard);
+async function counter() {
+  let goodSweets = await callingDesserts();
+  let flat = goodSweets.flat();
+  let cookie = flat.filter((oreo) => oreo.category == "Cookie").length;
+  let donuts = flat.filter((oreo) => oreo.category == "Donut").length;
+  let iceCream = flat.filter((oreo) => oreo.category == "Ice_Cream").length;
 
-  // Find the corresponding heart icon on the main page
-  const heart = document.querySelector(`[alt="${selectedCard.querySelector('#category').innerText}"]`).parentNode.querySelector('.fa-heart');
-
-  // Change the color of the heart icon back to blue
-  heart.classList.remove('red');
-  heart.classList.add('blue');
+  const numberCount = document.querySelector(".numberCount");
+  let sweetness = document.createElement("div");
+  sweetness.classList.add("sweetness");
+  sweetness.innerHTML = `
+  <h3>Total Amount of Cookies: ${cookie}</h3>
+  <h3>Total Amount of Donuts:  ${donuts} </h3>
+  <h3>Total Amount of Ice-Creams:   ${iceCream}</h3>`;
+  numberCount.appendChild(sweetness);
+  dessertsToDisplay();
 }
 
-
+counter();
